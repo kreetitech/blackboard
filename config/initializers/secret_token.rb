2bc68fc272d75077b7e4e3890e5ded2e1b4ea2ce
Priyashree <priyashree2810@gmail.com>
2011-06-21 10:19:29 +0530

    * Initial blackboard import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackboard::Application.config.secret_token = '045bc9efb9162f6a5f51d44b80ff420c3b5253cba2638e6ac1c73386afcce147ed58ed6964539b193a89644bc772a715700a1e72bdbde644989927cb8b1843de'

6470f8be7465ac12cb8b7869c10a7dbc4443dd8d
sanjana <sanjana@sanjana-laptop.(none)>
2011-06-28 19:29:30 +0530

    user-authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+puts 'SETTING UP ADMIN USERS'
+
+user1 = User.create!(:first_name => 'Admin1', :last_name => 'admin', :user_type => 3, :email => 'admin1@test.com', :password => 'admin123', :password_confirmation => 'admin123')
+
+puts 'New user created: ' << user1.first_name
+
+user2 = User.create!(:first_name => 'Admin2', :last_name => 'admin', :user_type => 3, :email => 'admin2@test.com', :password => 'admin123', :password_confirmation => 'admin123')
+
+puts 'New user created: ' << user2.first_name

647565998ebbb46d8215436009e14da8a46ba72b
sanjana <sanjana@sanjana-laptop.(none)>
2011-06-30 19:01:31 +0530

    views of department

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -14,20 +14,25 @@ class DepartmentsController < ApplicationController
 
   def edit
     @department = Department.find(params[:id])
+    @title = "Edit"
   end
 
   def update
     @department = Department.find(params[:id])
     @department.update_attributes(params[:department])
+    redirect_to departments_path
+    
   end
 
   def show
      @department = Department.find(params[:id])
+     @title = "Display"
   end
 
   def destroy
     @department = Department.find(params[:id])
     @department.destroy
+    redirect_to departments_path
   end
   
 end

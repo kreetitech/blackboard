efa282bf20d0bc413e55e499caf58cd11221f294
Priyashree <priyashree2810@gmail.com>
2011-06-30 15:21:49 +0530

    add header footer partials

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blackboard::Application.routes.draw do
 
   devise_for :users
 
-root :to => "home#index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

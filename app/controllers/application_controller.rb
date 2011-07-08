c9bf0b317cdf37c0d20bc903237a87e389b18c6d
Priyashree <priyashree2810@gmail.com>
2011-07-08 12:53:49 +0530

    devise after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  def after_sign_in_path_for(resource)
+    if current_user && current_user.admin?
+      departments_path
+    else
+      super
+    end
+  end
 end

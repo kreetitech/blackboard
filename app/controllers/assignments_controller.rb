4e7107e7f994ff0982333134dff0d919055e554f
anirban banerjee <092210@gmail.com>
2011-07-08 19:58:00 +0530

    corrected unchanged files

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,13 +1,42 @@
 class AssignmentsController < ApplicationController
 
    def index
-    @course = Course.find(params[:course_id])
+    @course = Course.find(params[:course_id => @course_id])
     @courseSessions = @course.courseSessions
   end
 
   def new
-     @course = Course.find(params[:id])
+  #  Assignment.create!(param[:assignment])
   end
-  
+ 
+ def new
+  #  Assignment.create!(param[:assignment])
+  end
+ 
+  def create
+   Assignment.create!(params[:assignment])
+  end
+ 
 
+  
+  def edit
+     @course = Course.find(params[:course_id])
+    @courseSession = @course.courseSessions(params[:course_session_id])
+    @assignment= @courseSession.assigments(params[:id])
+    @assigment.update_attributes(params[:assignment])
+    redirect_to assigments_path
+  end
+
+  def show 
+    @course = Course.find(params[:course_id])
+    @courseSessions = @course.courseSessions(params[:course_session_id])
+    @courseSession.assignments
+    redirect_to assigments_path
+  end
+
+  def delete
+   @assignment = Assignment.find(params[:id])
+    @assignent.destroy
+    redirect_to assignments_path
+  end
 end

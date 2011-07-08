073055743202b1536e5a668fecd26b8f4c39b646
Natasha Kedia <kedia.natasha9@gmail.com>
2011-07-08 19:01:32 +0530

    paperclip

--- app/models/member.rb
+++ app/models/member.rb
@@ -11,5 +11,8 @@ class Member < ActiveRecord::Base
 
   validates_presence_of :registration_id
 
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,7 @@ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-end
 
+
+Paperclip.options[:command_path] = "/usr/local/bin/"
+end

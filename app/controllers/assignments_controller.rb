e8fb46dba13b50b6d727cdd23caac157c623917c
Priyobroto Ghosh <ghoshpriyobroto@gmail.com>
2011-07-11 17:05:18 +0530

    changed veiws of asgnmnt & crse ssn

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,33 +1,37 @@
 class AssignmentsController < ApplicationController
 
    def index
-    @course = Course.find(params[:course_id => @course_id])
-    @courseSessions = @course.courseSessions
+    @course_session = CourseSession.find(params[:coursesession_id])
+    @assignments = @courseSessions.assignment
   end
 
   def new
-  #  Assignment.create!(param[:assignment])
+    @course_session =  CourseSession.find(params[:course_session_id])
   end
- 
- def new
-  #  Assignment.create!(param[:assignment])
-  end
- 
+
+
   def create
-   Assignment.create!(params[:assignment])
+    @course_session = CourseSession.find(params[:course_session_id])
+    assignment = @course_session.assignments.new(params[:assignments])
+    assignment.member_id = current_user.id
+    assignment.save!
+    redirect_to course_session_path(@course_session, :course_id => @course_session.course.id)
   end
- 
 
-  
+
   def edit
-     @course = Course.find(params[:course_id])
-    @courseSession = @course.courseSessions(params[:course_session_id])
+    @courseSession = CourseSession.find(params[:course_session_id])
     @assignment= @courseSession.assigments(params[:id])
-    @assigment.update_attributes(params[:assignment])
-    redirect_to assigments_path
   end
 
-  def show 
+  def update
+    coursesession = CourseSession.find(params[:coursesession_id])
+    assignment = course_session.assignments.find(params[:id])
+    assignment.update_attributes(params[:assignment])
+    redirect_to course_session_path(course_session,:course_id => course_session.course.id)
+
+  end
+  def show
     @course = Course.find(params[:course_id])
     @courseSessions = @course.courseSessions(params[:course_session_id])
     @courseSession.assignments
--- db/migrate/20110628123512_create_assignments.rb
+++ db/migrate/20110628123512_create_assignments.rb
@@ -1,18 +1,18 @@
 class CreateAssignments < ActiveRecord::Migration
   def self.up
     create_table :assignments do |t|
-      
+
       t.string :title
       t.text :description
       t.integer :full_marks
       t.datetime :due_date
       t.integer :member_id, :null => false
       t.integer :course_id, :null => false
-      
+
       t.timestamps
     end
 
-add_foreign_key :assignments, :course_id, :courses, :id
+    add_foreign_key :assignments, :course_id, :courses, :id
 
 
   end

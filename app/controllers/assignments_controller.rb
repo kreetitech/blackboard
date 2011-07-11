e575346633f98f618b92128b505a7708214a48c8
Abarna Sarkar <abarna.sarkar@gmail.com>
2011-07-11 15:01:46 +0530

    modified assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -11,7 +11,11 @@ class AssignmentsController < ApplicationController
  
  
   def create
-   Assignment.create!(params[:assignment])
+    @course_session = Course_session.find(params[:course_session_id])
+    @course_session.assignments.create!(params[:assignment])
+
+    redirect_to course_session_path(@course_session, :course_id => @course_session.course.id)
+
   end
  
 
@@ -28,12 +32,12 @@ class AssignmentsController < ApplicationController
     @course = Course.find(params[:course_id])
     @courseSessions = @course.courseSessions(params[:course_session_id])
     @courseSession.assignments
-    redirect_to assigments_path
+    redirect_to course_session_path(@course_session, :course_id => @course_session.course.id)
   end
 
   def delete
     @assignment = Assignment.find(params[:id])
     @assignent.destroy
-    redirect_to assignments_path
+    redirect_to course_session_path(@course_session, :course_id => @course_session.course.id)
   end
 end

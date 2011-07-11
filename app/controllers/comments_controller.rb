aea236e61cab8a159e2e0c80e83c6ea84c7bf49c
Abarna Sarkar <abarna.sarkar@gmail.com>
2011-07-11 16:08:10 +0530

    modified the assignments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,49 @@
 class CommentsController < ApplicationController
+
+  def index
+     @course = Course.find(params[:course_id])
+     @courseSessions = @course.courseSessions
+  end
+
+  def new
+     @course = Course.find(params[:course_id])
+  end
+
+  def edit
+     @course = Course.find(params[:course_id])
+     @course_session = @course.course_sessions.find(params[:id])
+  end
+
+  def create
+      @course = Course.find(params[:course_id])
+      @course.course_sessions.create!(params[:course_session])
+      redirect_to course_path(@course, :program_id => @course.program.id)
+  end
+
+
+  def update
+      course = Course.find(params[:course_id])
+      course_session = course.course_sessions.find(params[:id])
+      course_session.update_attributes(params[:course_session])
+      redirect_to course_path(course, :program_id => course.program.id)
+
+  end
+
+  def show
+    @course = Course.find(params[:course_id])
+    @program = @course.program
+    @department = @program.department
+    @course_session = @course.course_sessions.find(params[:id])
+    @gradebooks = @course_session.gradebooks
+    @assignments = @course_session.assignments
+  end
+
+  def destroy
+    @course = Course.find(params[:course_id])
+    @course_session = @course.course_sessions.find(params[:id])
+    @course_session.destroy
+    redirect_to course_path(@course, :program_id => @course.program.id)
+  end
 end
+
+
--- db/migrate/20110628123512_create_assignments.rb
+++ db/migrate/20110628123512_create_assignments.rb
@@ -7,12 +7,12 @@ class CreateAssignments < ActiveRecord::Migration
       t.integer :full_marks
       t.datetime :due_date
       t.integer :member_id, :null => false
-      t.integer :course_session_id, :null => false
+      t.integer :course_id, :null => false
       
       t.timestamps
     end
 
-add_foreign_key :assignments, :course_session_id, :course_sessions, :id
+add_foreign_key :assignments, :course_id, :courses, :id
 
 
   end
